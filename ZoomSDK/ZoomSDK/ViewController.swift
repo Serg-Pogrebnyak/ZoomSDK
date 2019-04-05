68c391628a4d688b14a55881f4c9e1da095e2af1
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2019-04-05 15:16:32 +0300

    start project

---
+++ ZoomSDK/ZoomSDK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZoomSDK
+//
+//  Created by Sergey Pohrebnuak on 4/5/19.
+//  Copyright © 2019 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
